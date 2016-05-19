c1fee73ad1d5a372ea25c58ec59f3c4a86c74dd8
Faisal Hameed <faisal.hameed@devfactory.com>
2016-05-19 12:42:33 +0500

    Fixing squid:S1066 - Collapsible "if" statements should be merged.

--- app/src/main/java/bf/io/openshop/api/JsonRequest.java
+++ app/src/main/java/bf/io/openshop/api/JsonRequest.java
@@ -110,12 +110,10 @@ public class JsonRequest extends JsonObjectRequest {
                 Timber.e("%s URL: %s. ERROR: %s", this.getClass().getSimpleName(), requestUrl, new String(volleyError.networkResponse.data));
 
             // If AccessToken expired. Logout user and redirect to home page.
-            if (getStatusCode() == HttpURLConnection.HTTP_FORBIDDEN) {
-                if (fragmentManager != null) {
-                    LoginDialogFragment.logoutUser();
-                    DialogFragment loginExpiredDialogFragment = new LoginExpiredDialogFragment();
-                    loginExpiredDialogFragment.show(fragmentManager, LoginExpiredDialogFragment.class.getSimpleName());
-                }
+            if (getStatusCode() == HttpURLConnection.HTTP_FORBIDDEN && fragmentManager != null) {
+                LoginDialogFragment.logoutUser();
+                DialogFragment loginExpiredDialogFragment = new LoginExpiredDialogFragment();
+                loginExpiredDialogFragment.show(fragmentManager, LoginExpiredDialogFragment.class.getSimpleName());
             }
         } else {
             requestStatusCode = CONST.MissingStatusCode;
